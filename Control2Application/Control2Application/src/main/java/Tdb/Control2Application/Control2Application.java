9529fe5b0226b8357ea625633b475e33305b7c54
BenjaminMoya <benjamin.moya.m@usach.cl>
2024-11-04 23:34:52 -0300

    First commit

---
+++ Control2Application/Control2Application/src/main/java/Tdb/Control2Application/Control2Application.java
@@ -0,0 +1,13 @@
+package Tdb.Control2Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Control2Application.class, args);
+	}
+
+}
---
+++ Control2Application/Control2Application/src/test/java/Tdb/Control2Application/Control2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Tdb.Control2Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
